d936ea92707fab4684da620fcee0bef77e8ad2ff
Yevgeniy <evg.develop@gmail.com>
2020-06-22 01:15:41 +0500

    initial

---
+++ android/src/ru/mavr/AndroidLauncher.java
@@ -0,0 +1,17 @@
+package ru.mavr;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+
+import ru.mavr.MavrGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MavrGame(), config);
+	}
+}
---
+++ core/src/ru/mavr/MavrGame.java
@@ -0,0 +1,26 @@
+package ru.mavr;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+//import com.badlogic.gdx.Game;
+
+public class MavrGame extends Game {
+	protected Screen screen;
+
+	@Override
+	public void create() {
+	}
+
+	@Override
+	public void render() {
+	}
+
+	@Override
+	public void dispose() {
+	}
+}
